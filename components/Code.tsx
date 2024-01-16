8856f1faaee63179174b925dc7e44c19a2f26162
eraykeskinmac <eraykeskinmac@gmail.com>
2024-01-17 00:36:11 +0300

    refactor: enhance Code component with new hooks and styles

--- components/Code.tsx
+++ components/Code.tsx
@@ -1,25 +1,99 @@
 "use client";
-import { useState } from "react";
+
+import clsx from "clsx";
+import { Highlight, themes } from "prism-react-renderer";
+import { useEffect, useRef, useState } from "react";
 
 interface CodeProps {
   placeholder?: string;
-  initialValue?: string;
+  initialValue: string;
 }
 
 export default function Code({ placeholder, initialValue }: CodeProps) {
+  const preRef = useRef<HTMLPreElement>(null);
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+
   const [value, setValue] = useState(initialValue);
+  const [isTextAreaFocuses, setIsTextAreaFocuses] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (textAreaRef.current) {
+      textAreaRef.current.focus();
+    }
+  }, []);
+
+  useEffect(() => {
+    if (containerRef.current && preRef.current && textAreaRef.current) {
+      const containerHeight = containerRef.current.clientHeight;
+      const preHeight = preRef.current.clientHeight;
+
+      textAreaRef.current.style.height = `${Math.max(
+        containerHeight,
+        preHeight
+      )}px`;
+    }
+  }, [containerRef.current?.clientHeight, preRef.current?.clientHeight]);
 
   const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setValue(event.target.value);
   };
 
   return (
-    <textarea
-      placeholder={placeholder}
-      value={value}
-      onChange={handleChange}
-      rows={30}
-      className="w-1/2 resize-none"
-    ></textarea>
+    <div
+      className={clsx(
+        isTextAreaFocuses ? "border-pink-400" : "border-white/20",
+        "h-2/3 w-2/3 max-w-4xl rounded-xl border-[1px] py-4",
+        "transition-colors duration-300 ease-in-out"
+      )}
+    >
+      <div ref={containerRef} className="relative h-full w-full overflow-auto">
+        <Highlight theme={themes.nightOwl} code={value} language="jsx">
+          {({ className, tokens, getLineProps, getTokenProps }) => (
+            <>
+              <textarea
+                ref={textAreaRef}
+                value={value}
+                placeholder="Add some code here..."
+                onChange={handleChange}
+                spellCheck={false}
+                onFocus={() => setIsTextAreaFocuses(true)}
+                onBlur={() => setIsTextAreaFocuses(false)}
+                className={clsx(
+                  className,
+                  "absolute w-full resize-none overflow-hidden whitespace-pre-wrap break-words break-keep bg-transparent pl-16 pr-3 font-mono text-transparent",
+                  "caret-pink-500 selection:bg-pink-500/30 placeholder:text-white/20 focus:outline-none"
+                )}
+              />
+              <pre
+                ref={preRef}
+                className={clsx(
+                  className,
+                  "pointer-events-none absolute w-full select-none pr-3"
+                )}
+                aria-hidden={true}
+              >
+                {tokens.map((line, i) => (
+                  <div
+                    key={i}
+                    {...getLineProps({ line, key: i })}
+                    className="table-row"
+                  >
+                    <span className="table-cell w-10 select-none text-right opacity-50">
+                      {i + 1}
+                    </span>
+                    <code className="table-cell whitespace-pre-wrap break-words break-keep pl-6">
+                      {line.map((token, key) => (
+                        <span key={key} {...getTokenProps({ token, key })} />
+                      ))}
+                    </code>
+                  </div>
+                ))}
+              </pre>
+            </>
+          )}
+        </Highlight>
+      </div>
+    </div>
   );
 }
