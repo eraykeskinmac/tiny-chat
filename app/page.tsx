5191dfe1fd2a70ed1c75a3e7a7ba9cf9ffd1af89
eraykeskinmac <eraykeskinmac@gmail.com>
2024-01-17 00:36:10 +0300

    fix: update initial value of Code component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 import Code from "@/components/Code";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="h-full flex items-center justify-center flex-col gap-6">
       <h1 className="text-4xl font-black">Code Share</h1>
-      <Code placeholder="Paster some code here..." />
+      <Code initialValue="test" />
     </main>
   );
 }
