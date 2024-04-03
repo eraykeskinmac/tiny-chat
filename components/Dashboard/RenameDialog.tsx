41d46cc0eeae3896e3acf68048986914a55a2314
eraykeskinmac <eraykeskinmac@gmail.com>
2024-04-03 09:09:12 +0300

    style: fix typo in class names and update button border color

--- components/Dashboard/RenameDialog.tsx
+++ components/Dashboard/RenameDialog.tsx
@@ -37,7 +37,8 @@ export default memo(function RenameDialog({
         className={cn(
           "w-full truncate rounded-md p-1 text-lg",
           "outline-none",
-          "bg-transparent text-almost-white",
+          "bg-transparent text-amlost-white",
+          "transition-all duration-100 ease-in-out",
           "placeholder:text-almost-white/50",
           "focus:placeholder:text-transparent",
           !localInputValue && "italic"
@@ -52,9 +53,9 @@ export default memo(function RenameDialog({
             className={cn(
               "mt-4 w-full rounded-lg p-3",
               "select-none outline-none",
-              "border border-white/20 bg-white/10 text-almost-white",
+              "border border-white/20 bg-white/10 text-amlost-white",
               "transition-all duration-100 ease-in-out",
-              "focus:border-almost-white",
+              " focus:border-amlost-white",
               "disabled:cursor-not-allowed disabled:brightness-50"
             )}
           >
@@ -64,42 +65,16 @@ export default memo(function RenameDialog({
               ) : (
                 <Edit3 size={16} aria-hidden="true" />
               )}
-              <span className="min-w-fit">Rename Snippet</span>
-              <span
-                className={cn("truncate font-medium", "text-greylish")}
-              ></span>
+              <span className="min-w-fit">Rename snippet to</span>
+              <span className={cn("truncate font-medium", "text-greyish")}>
+                {`"${localInputValue}"`}
+              </span>
             </div>
           </button>
         </DialogPrimitive.Close>
       )}
-      <DialogPrimitive.Description>
-        Deleted Snippet can&apos;t be recovered.
-      </DialogPrimitive.Description>
       <div className={cn("mt-4 flex flex-row justify-end")}>
-        <DialogPrimitive.Close asChild>
-          <button
-            type="button"
-            onClick={() => action({ id })}
-            disabled={isLoading}
-            className={cn(
-              "rounded-lg p-3 font-medium",
-              "select-none outline-none",
-              "border border-red-500/40 bg-black",
-              "transition-all duration-100 ease-in-out",
-              "hover:bg-red-500/30 hover:text-almost-white",
-              "focus:border-red-500 focus:bg-red-500/30 focus:text-almost-white",
-              "disabled:cursor-not-allowed disabled:brightness-50"
-            )}
-          >
-            <div className={cn("flex items-center gap-2")}>
-              {isLoading ? (
-                <Loader extraClasses="h-4 w-4" />
-              ) : (
-                <Trash size={16} aria-hidden="true" />
-              )}
-            </div>
-          </button>
-        </DialogPrimitive.Close>
+        <DialogPrimitive.Close asChild></DialogPrimitive.Close>
       </div>
     </form>
   );
