d0dc4ae1125a3ef0c16591e8e841446a463ee383
eraykeskinmac <eraykeskinmac@gmail.com>
2024-04-21 02:32:07 +0300

    Refactor import statements

--- components/Editor/TitleBar.tsx
+++ components/Editor/TitleBar.tsx
@@ -1,9 +1,12 @@
-import { cn } from "@/lib/cn";
-import { useStore } from "@/lib/store";
-import { useRef } from "react";
-import { useHotkeys } from "react-hotkeys-hook";
-import Tooltip from "../ui/Tooltip";
-import { Info } from "lucide-react";
+import { useRef } from 'react';
+
+import { Info } from 'lucide-react';
+import { useHotkeys } from 'react-hotkeys-hook';
+
+import { cn } from '@/lib/cn';
+import { useStore } from '@/lib/store';
+
+import Tooltip from '../ui/Tooltip';
 
 export default function TitleBar({ editable = false }: { editable: boolean }) {
   const inputRef = useRef<HTMLInputElement>(null);
@@ -12,7 +15,7 @@ export default function TitleBar({ editable = false }: { editable: boolean }) {
   const update = useStore((state) => state.update);
 
   useHotkeys(
-    "t",
+    't',
     () => {
       if (inputRef.current) {
         inputRef.current.focus();
@@ -22,54 +25,54 @@ export default function TitleBar({ editable = false }: { editable: boolean }) {
     {
       enabled: editable,
       preventDefault: true,
-    }
+    },
   );
 
   return (
     <div
       className={cn(
-        "relative flex items-center justify-center gap-2 rounded-t-lg py-2",
-        "bg-black/40"
+        'relative flex items-center justify-center gap-2 rounded-t-lg py-2',
+        'bg-black/40',
       )}
     >
       <Tooltip
         content={
-          <div className={cn("flex items-center gap-2 pl-1 text-xs")}>
+          <div className={cn('flex items-center gap-2 pl-1 text-xs')}>
             <Info size={16} aria-hidden="true" />
           </div>
         }
-        kbd={["T"]}
+        kbd={['T']}
         disabled={!editable}
       >
         <input
           type="text"
           ref={inputRef}
-          value={title ?? ""}
+          value={title ?? ''}
           spellCheck={false}
           autoComplete="off"
           maxLength={70}
-          onChange={(e) => update("title", e.target.value)}
+          onChange={(e) => update('title', e.target.value)}
           disabled={!editable}
           tabIndex={-1}
           className={cn(
-            "w-32 truncate rounded-md text-center font-medium leading-loose",
-            "outlone-none",
-            "bg-transparent text-almost-white/40",
-            "transition-all duration-100 ease-in-out",
-            "focus:text-almost-white"
+            'w-32 truncate rounded-md text-center font-medium leading-loose',
+            'outlone-none',
+            'bg-transparent text-almost-white/40',
+            'transition-all duration-100 ease-in-out',
+            'focus:text-almost-white',
           )}
           aria-label="title-input"
         />
       </Tooltip>
       <div
         className={cn(
-          "absolute -bottom-2 left-0 flex w-full justify-between fill-black/30"
+          'absolute -bottom-2 left-0 flex w-full justify-between fill-black/30',
         )}
       >
-        <svg width={8} height={8} className={cn("rotate-180", "fill-black/40")}>
+        <svg width={8} height={8} className={cn('rotate-180', 'fill-black/40')}>
           <path d="M0 8a8 8 0 0 0 0 8-8v8H0Z" />
         </svg>
-        <svg width={8} height={8} className={cn("rotate-180", "fill-black/40")}>
+        <svg width={8} height={8} className={cn('rotate-180', 'fill-black/40')}>
           <path d="M0 8a8 8 0 0 0 0 8-8v8H0Z" />
         </svg>
       </div>
