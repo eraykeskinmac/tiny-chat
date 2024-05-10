05b5e2012ab2acfa788b28b5d4fa6fc5dab3c6ba
eraykeskinmac <eraykeskinmac@gmail.com>
2024-05-10 04:08:18 +0300

    Refactor Select component to improve code readability and maintainability

--- components/Select.tsx
+++ components/Select.tsx
@@ -1,55 +1,62 @@
+import { Fragment, memo } from 'react';
+
+import { Listbox, Transition } from '@headlessui/react';
+import { ChevronDownIcon } from '@radix-ui/react-icons';
+import clsx from 'clsx';
+
 import {
   FontDefinition,
   LanguageDefinition,
   ThemeDefinition,
-} from "@/lib/types";
-import { Listbox, Transition } from "@headlessui/react";
-import { ChevronDownIcon } from "@radix-ui/react-icons";
-import clsx from "clsx";
-import { Fragment, memo } from "react";
+} from '@/lib/types';
 
 interface SelectProps<T> {
-  type: "language" | "theme" | "font";
+  type: 'language' | 'theme' | 'font';
   initialValue: T;
   setValue: (_: T) => void;
   options: T[];
 }
 
-export function ThemeBubble({ color }: { color: string }) {
+function ThemeBubble({ color }: { color: string }) {
   return (
     <span
-      className={clsx("block h-4 w-4 rounded-full bg-gradient-to-br", color)}
+      className={clsx('block h-4 w-4 rounded-full bg-gradient-to-br', color)}
     />
   );
 }
 
 export default memo(function Select<
-  T extends ThemeDefinition | LanguageDefinition | FontDefinition
+  T extends ThemeDefinition | LanguageDefinition | FontDefinition,
 >({ type, initialValue, setValue, options }: SelectProps<T>) {
   const getClassName = (fontVariable: string) => {
     switch (fontVariable) {
-      case "var(--font-fira-code)":
-        return "font-firaCode";
-      case "var(--font-jetbrains-mono)":
-        return "font-jetBrainsMono";
-      case "var(--font-incosolata)":
-        return "font-inconsolata";
-      case "var(--font-source-code-pro)":
-        return "font-sourceCodePro";
+      case 'var(--font-fira-code)':
+        return 'font-firaCode';
+      case 'var(--font-jetbrains-mono)':
+        return 'font-jetBrainsMono';
+      case 'var(--font-inconsolata)':
+        return 'font-inconsolata';
+      case 'var(--font-source-code-pro)':
+        return 'font-sourceCodePro';
       default:
-        return "font-mono";
+        return 'font-mono';
     }
   };
 
   const getInitialValue = (type: string) => {
     switch (type) {
-      case "language":
+      case 'language':
         return <span>{(initialValue as LanguageDefinition).label}</span>;
-      case "theme":
+      case 'theme':
         return <ThemeBubble color={(initialValue as ThemeDefinition).class} />;
-      case "font":
+      case 'font':
         return (
-          <span className={clsx((initialValue as FontDefinition).class)}>
+          <span
+            className={clsx(
+              // getClassName((initialValue as FontDefinition).variable)
+              (initialValue as FontDefinition).class,
+            )}
+          >
             {(initialValue as FontDefinition).label}
           </span>
         );
@@ -57,18 +64,19 @@ export default memo(function Select<
         return null;
     }
   };
+
   const getOptionContent = (
     type: string,
-    option: ThemeDefinition | LanguageDefinition | FontDefinition
+    option: LanguageDefinition | ThemeDefinition | FontDefinition,
   ) => {
     switch (type) {
-      case "language":
+      case 'language':
         return (
           <span className="block truncate pr-9">
             {(option as LanguageDefinition).label}
           </span>
         );
-      case "theme":
+      case 'theme':
         return (
           <>
             <ThemeBubble color={(option as ThemeDefinition).class} />
@@ -77,7 +85,7 @@ export default memo(function Select<
             </span>
           </>
         );
-      case "font":
+      case 'font':
         return (
           <span className={clsx((initialValue as FontDefinition).class)}>
             {(option as FontDefinition).class}
@@ -87,22 +95,21 @@ export default memo(function Select<
         return null;
     }
   };
-
   return (
     <Listbox value={initialValue} onChange={setValue}>
       <div className="relative">
         <Listbox.Button
           className={clsx(
-            "flex w-auto select-none items-center justify-between gap-3 rounded-lg p-2 text-xs",
-            "border-[1px] border-white/20 bg-black",
-            "transition-colors duration-200 ease-in-out",
-            "hover:cursor-pointer hover:bg-white/20 focus:outline-none",
-            type === "language" && "w-32 ",
-            type === "font" && "w-40 "
+            'flex w-auto select-none items-center justify-between gap-3 rounded-lg p-2 text-xs',
+            'border-[1px] border-white/20 bg-black',
+            'transition-colors duration-200 ease-in-out',
+            'hover:cursor-pointer hover:bg-white/20 focus:outline-none',
+            type === 'language' && 'w-32',
+            type === 'font' && 'w-40',
           )}
         >
           {getInitialValue(type)}
-          <span className="pointer-event-none">
+          <span className="pointer-events-none">
             <ChevronDownIcon className="h-3 w-3" aria-hidden="true" />
           </span>
           <Transition
@@ -116,9 +123,9 @@ export default memo(function Select<
           >
             <Listbox.Options
               className={clsx(
-                "absolute z-10 max-h-80 -translate-x-1/4 -translate-y-3/4 space-y-1 overflow-auto rounded-xl p-2",
-                "border-[1px] border-white/20 bg-black",
-                "focus-outline-none"
+                'absolute z-10 max-h-80 -translate-x-1/4 -translate-y-3/4 space-y-1 overflow-auto rounded-xl p-2',
+                'border-[1px] border-white/20 bg-black',
+                'focus:outline-none',
               )}
             >
               {options.map((option, i) => (
@@ -126,9 +133,9 @@ export default memo(function Select<
                   key={`${type}-${i}`}
                   value={option}
                   className={clsx(
-                    "flex items-center gap-3 rounded-lg p-2 text-xs",
-                    "cursor-pointer select-none",
-                    "transition-colors duration-200 ease-in-out"
+                    'flex items-center gap-3 rounded-lg p-2 text-xs',
+                    'cursor-pointer select-none',
+                    'transition-colors duration-200 ease-in-out',
                   )}
                 >
                   {getOptionContent(type, option)}
