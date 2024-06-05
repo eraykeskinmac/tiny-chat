aa7a74d9ff092faef091c62b0fe54bd60a654021
Mustafa Eray <eraykeskinmac@gmail.com>
2024-06-05 14:36:47 +0300

    feat: Add Loading component for dashboard

---
+++ components/ui/Skeleton.tsx
@@ -0,0 +1,12 @@
+import { cn } from '@/lib/cn';
+
+export default function Skeleton({ className }: { className?: string }) {
+  return (
+    <div
+      className={cn(
+        'rounded-md,"bg-almost-white/10","animate-pulse"',
+        className,
+      )}
+    />
+  );
+}
