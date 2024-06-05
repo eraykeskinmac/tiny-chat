addaeaf4c6dc1d38d541a3db0cb4d37070e2a954
Mustafa Eray <eraykeskinmac@gmail.com>
2024-06-05 10:27:40 +0300

    refactor: Add useRef hook to improve drag and drop functionality in Settings component

--- components/Settings/index.tsx
+++ components/Settings/index.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from 'react';
+import { useMemo, useRef, useState } from 'react';
 
 import {
   AnimatePresence,
@@ -6,6 +6,7 @@ import {
   MotionValue,
   useAnimationControls,
   useDragControls,
+  useMotionValue,
   useMotionValueEvent,
 } from 'framer-motion';
 import { GripHorizontal, RefreshCcw } from 'lucide-react';
@@ -13,6 +14,7 @@ import { GripHorizontal, RefreshCcw } from 'lucide-react';
 import { cn } from '@/lib/cn';
 import { SUPPORTED_FONT_STYLES } from '@/lib/fonts';
 import { SUPPORTED_LANGUAGES } from '@/lib/languages';
+import { useStore } from '@/lib/store';
 import { SUPPORTED_THEMES } from '@/lib/theme';
 import {
   BASE_COLOR_MODES,
@@ -20,11 +22,82 @@ import {
   BASE_PADDING_VALUES,
 } from '@/lib/values';
 
+import Actions from './Actions';
 import Angle from './Angel';
 import Choices from './Choices';
 import Picker from './Picker';
 import Select from './Select';
 import Switch from './Switch';
+
+export default function Settings() {
+  const [hasMoved, setHasMoved] = useState(false);
+
+  const constraintsRef = useRef(null);
+
+  const dragControls = useDragControls();
+  const animationControls = useAnimationControls();
+  const isDragging = useMotionValue(false);
+
+  const hasCustomTheme = useStore((state) => state.hasCustomTheme);
+
+  return (
+    <>
+      <div
+        key={`settings-expanded-${hasCustomTheme}`}
+        ref={constraintsRef}
+        className={cn(
+          'fixed bottom-6 left-6 right-6 top-[88px] -z-10',
+          'pointer-events-none',
+        )}
+      >
+        <motion.div
+          id="settings"
+          drag
+          dragListener={false}
+          dragMomentum={false}
+          dragControls={dragControls}
+          dragConstraints={constraintsRef}
+          onDragStart={() => isDragging.set(true)}
+          onDragEnd={(_, info) => {
+            isDragging.set(false);
+            if (!hasMoved && (info.point.x !== 0 || info.point.y !== 0)) {
+              setHasMoved(true);
+            }
+          }}
+          animate={animationControls}
+          onUpdate={({ x, y }) => {
+            if (hasMoved && x === 0 && y === 0) {
+              setHasMoved(false);
+            }
+          }}
+          className={cn(
+            'fixed bottom-12 z-40 w-[960px] rounded-xl font-medium',
+            'border border-white/20 bg-black/50 shadow-xl shadow-black/40 backdrop-blur-md',
+          )}
+        >
+          <DraggableHandle
+            dragControls={dragControls}
+            animationControls={animationControls}
+            hasMoved={hasMoved}
+            isDragging={isDragging}
+          />
+          <div
+            className={cn(
+              'relative flex-col overflow-hidden flex rounded-xl px-4 pb-4 pt-5',
+              'border-b border-white/20 bg-black shadow-xl shadow-black/40',
+              'transition-all duration-100 ease-in-out',
+              hasCustomTheme ? 'h-[211px]' : 'h-[97px]',
+            )}
+          >
+            <BasicSnippetControls />
+            {hasCustomTheme && <CustomThemeControls />}
+          </div>
+          <Actions />
+        </motion.div>
+      </div>
+    </>
+  );
+}
 
 function DraggableHandle({
   dragControls,
