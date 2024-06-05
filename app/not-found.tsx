7a3c5869e14e8bc2a8978f79c3cc1bf944519e26
Mustafa Eray <eraykeskinmac@gmail.com>
2024-06-05 14:51:39 +0300

    refactor: Add NotFound component for displaying 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+import { cn } from '@/lib/cn';
+
+export default function NotFound() {
+  return (
+    <div className={cn('flex items-center justify-center')}>
+      <h1 className="text-4xl font-extrabold">404</h1>
+    </div>
+  );
+}
